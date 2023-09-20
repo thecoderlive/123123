e6418e82503dc4f930482b7a7541e52d8d8c169b
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-20 14:21:47 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "c00aa86a-1b9d-4776-8c60-0894dd25e171",
+    "home_feed": [
+        {
+            "id": "e356d4ae-6335-4476-a43f-79e5aeb2065b",
+            "artist_name": "see",
+            "home_carousel": [
+                {
+                    "id": "310ae6ad-c302-4809-b0b1-5ef76baa9125",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "321964cb-5dc5-4b35-82fc-4746178879ba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "46b69230-7933-43b7-b1ea-79fdb86d96d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+                },
+                {
+                    "id": "e9d1820a-1b3f-4977-a386-473356cfd357",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                },
+                {
+                    "id": "caebb8e9-7891-460b-92d1-ac703f6b1786",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                }
+            ],
+            "art_caption": "second",
+            "art_description": "Dinner person find continue degree. Option heavy foreign news blood capital quite their."
+        },
+        {
+            "id": "1333850f-dca6-4a48-b4b2-5427563c9865",
+            "artist_name": "believe",
+            "home_carousel": [
+                {
+                    "id": "9c219afb-cd75-4606-bfcb-a67bed25c115",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "ae92760d-b5e1-40d6-9cdf-f460e4275e4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "8cce1457-e20b-4397-818a-d41ddf0750b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "ce6f3f25-622f-4e57-bf99-37e17688f07d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "b8e66108-d824-435f-a40e-ac2e02bf9028",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                }
+            ],
+            "art_caption": "bill",
+            "art_description": "Always consider fund decision decide. Month get whatever close war key. Senior just environment. Decade bag others dog."
+        },
+        {
+            "id": "56ef87b0-243e-471c-bde8-22f12ad392bb",
+            "artist_name": "sport",
+            "home_carousel": [
+                {
+                    "id": "69a46284-fb97-42f4-a636-c6406c45d17d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "e88915fe-3719-4021-98ed-2cd4116c7563",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "7df54889-8ef7-450b-b7da-65219ea0c1b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "60cdfb7f-6112-47d0-8732-9be73bd04f5f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "61e2985d-deb6-41d4-bf08-2518bd861c2f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                }
+            ],
+            "art_caption": "manager",
+            "art_description": "Fact head building appear send. Measure prepare ability sister staff describe. Else thousand mention center."
+        },
+        {
+            "id": "a69ce91e-b134-4c0e-a9ba-86b8e413d16b",
+            "artist_name": "road",
+            "home_carousel": [
+                {
+                    "id": "f1d3d629-b2f3-40fb-9230-09a25003ba97",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "89802b8e-8cf9-41fc-ab5a-5f82126668a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "65e5d7a7-1d5a-4375-ba27-c096b589ac35",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "40329ea5-387c-46c4-b4d5-3bb1cb78f923",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "54d1d848-a6e1-4226-8c11-6ed00a5f8c05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "let",
+            "art_description": "Talk wonder important pass event away. Moment seem marriage artist night."
+        },
+        {
+            "id": "5335dc86-da26-4826-8f56-929f3447f460",
+            "artist_name": "tree",
+            "home_carousel": [
+                {
+                    "id": "c83bc75e-dc79-49e3-9a5b-bef7f7bfdfe5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "75b6284d-ada4-47a4-805b-767fca6c5311",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "7be981b9-c95c-4ba1-96f2-edf504da231b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "efbc03d0-3f34-466a-b264-a8b824a606db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "b872b9a8-2c23-4c14-9fe7-3bb8fb5d1e29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                }
+            ],
+            "art_caption": "poor",
+            "art_description": "Write quickly beat quickly must picture. Pattern institution short road authority."
+        }
+    ]
+}
